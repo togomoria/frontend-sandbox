a83ad3d27df0d6b10050b6bfdb45ea653c29ddea
togomoria <i.togo.mori@globis.co.jp>
2024-02-22 09:21:54 +0900

    style: 見た目の作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import { Box, Button, TextField, TextareaAutosize } from '@mui/material'
+import axios from 'axios'
 import React from 'react'
 
-export const App: React.FC = () => (
-  <form
-    style={{
-      height: '200px',
-    }}
-  >
-    <textarea />
-  </form>
-)
+export const App: React.FC = () => {
+  const [message, setMessage] = React.useState<string>('')
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    axios.post('http://localhost:3000/api/messages', {
+      message,
+    })
+  }
+
+  return (
+    <Box>
+      <form>
+        <TextareaAutosize
+          minRows={5}
+          style={{
+            width: '100%',
+          }}
+          onChange={e => setMessage(e.target.value)}
+        />
+        <Box width="100%" display="flex">
+          <Button
+            variant="contained"
+            style={{
+              marginLeft: 'auto',
+            }}
+          >
+            送信
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  )
+}
